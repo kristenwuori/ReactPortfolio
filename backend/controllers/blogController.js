653de736a1552473f9294786951861ee2c3ec782
kristenwuori <105729385+kristenwuori@users.noreply.github.com>
2022-06-06 08:31:14 -0700

    Goal controller and routes set up

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,36 +1,38 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getBlogs = (req, res) => {
+const getBlogs = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 // @desc Set goals
 // @route POST /api/goals
 // @access Private
-const setBlog = (req, res) => {
+const setBlog = asyncHandler(async (req, res) => {
     if(!req.body.text) {
         res.status(400).json({ message: 'Please add a text field' })
     }
 
     res.status(200).json({ message: 'Set goal' })
-}
+})
 
 
 // @desc Update goals
 // @route PUT /api/goals
 // @access Private
-const updateBlog = (req, res) => {
+const updateBlog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` })
-}
+})
 
 
 // @desc Delete goals
 // @route DELETE /api/goals
 // @access Private
-const deleteBlog = (req, res) => {
+const deleteBlog = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}` })
-}
+})
 
 
 
