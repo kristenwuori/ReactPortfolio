a7da28a1350417a32eb07a475f7d55caa8845c6c
kristenwuori <105729385+kristenwuori@users.noreply.github.com>
2022-06-22 15:36:11 -0700

    another commit

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = 'http://localhost:8000/api/users'
+const API_URL = 'http://localhost:8000/api/users/'
 
 // Register user
 const register = async (userData) => {
